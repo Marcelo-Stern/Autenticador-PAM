a2cb9eef154526944d4970f347dd3771f3aa50a2
Marcelo-Stern <marcelo.silva776@etec.sp.gov.br>
2022-08-01 09:56:52 -0300

    First Commit

---
+++ app/src/main/java/com/example/autenticador/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.autenticador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth
+
+    }
+}
