d5b67497911a9ce1e54aac7e47d22b0f42cdff9b
Marcelo-Stern <marcelo.silva776@etec.sp.gov.br>
2022-08-29 09:09:32 -0300

    Finalizado com erro

--- app/src/main/java/com/example/autenticador/MainActivity.kt
+++ app/src/main/java/com/example/autenticador/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         val txtSenha: TextView = findViewById(R.id.txtSenha)
 
         if(txtEmail.text.isEmpty() || txtSenha.text.isEmpty()){
-            Toast.makeText(this, "Por Favor preencher os campos para efetuar o login!", Toast.LENGTH_SHORT)
+            Toast.makeText(this, "Por Favor preencher os campos para efetuar o login!", Toast.LENGTH_SHORT).show()
             return
         }
 
@@ -54,9 +54,9 @@ class MainActivity : AppCompatActivity() {
                     val intent = Intent(this, PrincipalActivity::class.java)
                     startActivity(intent)
 
-                    Toast.makeText(this, "Autenticação realizada com sucesso!", Toast.LENGTH_SHORT)
+                    Toast.makeText(this, "Autenticação realizada com sucesso!", Toast.LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(this, "Usuário e senha não conferem!", Toast.LENGTH_SHORT)
+                    Toast.makeText(this, "Usuário e senha não conferem!", Toast.LENGTH_SHORT).show()
                 }
             }
     }
--- app/src/main/java/com/example/autenticador/RegisterActivity.kt
+++ app/src/main/java/com/example/autenticador/RegisterActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-private lateinit var auth: FirebaseAuth
-
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -28,27 +29,30 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun performSignUp() {
-        val txtEmail: TextView = findViewById(R.id.edtLogin)
-        val txtSenha: TextView = findViewById(R.id.edtSenha)
+        val txtEmail: EditText = findViewById(R.id.edtLogin)
+        val txtSenha: EditText = findViewById(R.id.edtSenha)
 
         if(txtEmail.text.isEmpty() || txtSenha.text.isEmpty()){
-            Toast.makeText(this, "Por Favor preencher os campos para efetuar o login!", Toast.LENGTH_SHORT)
+            Toast.makeText(this, "Por Favor preencher os campos para relizar o cadastro!", Toast.LENGTH_SHORT).show()
             return
         }
 
         val inputEmail = txtEmail.text.toString()
         val inputSenha = txtSenha.text.toString()
 
-        auth.signInWithEmailAndPassword(inputEmail, inputSenha)
+        auth.createUserWithEmailAndPassword(inputEmail, inputSenha)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    val intent = Intent(this, PrincipalActivity::class.java)
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
 
-                    Toast.makeText(this, "Autenticação realizada com sucesso!", Toast.LENGTH_SHORT)
+                    Toast.makeText(baseContext, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(this, "Usuário e senha não conferem!", Toast.LENGTH_SHORT)
+                    Toast.makeText(baseContext, "O cadastro falhou!", Toast.LENGTH_SHORT).show()
                 }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Ocorreu o erro ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
             }
     }
 }
